ba78125abe6c2ade3481f73a05e3f84c93e80455
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2025-03-28 11:20:56 -0400

    init login and registration frontend feasure

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,11 +6,16 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./router/home.jsx"
+import LoginRegister from './router/Login_Register.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/loginregister',
+    element: <LoginRegister/>
   }
 ])
 
