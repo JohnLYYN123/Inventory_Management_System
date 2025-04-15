dad3a8070db6e278c321fe2abc38a1db28410ba3
chujing-yu <156983284+chujing-yu@users.noreply.github.com>
2025-04-15 00:10:49 -0400

    Update spaces endpoint

--- Backend/src/utils/doSpaces.js
+++ Backend/src/utils/doSpaces.js
@@ -1,7 +1,7 @@
 // Put the DigitalOcean Spaces configuration here
 const AWS = require('aws-sdk');
 
-const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com') // Update region if needed
+const spacesEndpoint = new AWS.Endpoint('tor1.digitaloceanspaces.com') // Update region if needed
 const s3 = new AWS.S3({
     endpoint: spacesEndpoint,
     accessKeyId: process.env.DO_SPACE_KEY,
